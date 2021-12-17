345a087bd2f3dde233deb48ad680cb9678fe92cf
Serge Maia <sergemaia9@gmail.com>
2021-12-17 11:44:27 +0000

    CFG: Project developed so far

---
+++ Souls Like Archive/Assets/Scripts/Controller/CameraManager.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+
+namespace Arkayns {
+
+    public class CameraManager : MonoBehaviour {
+
+        public static CameraManager singleton;
+
+        public bool lockOn;
+        public float followSpeed = 9;
+        public float mouseSpeed = 2;
+        public float controllerSpeed = 5;
+
+        public Transform target;
+
+        [HideInInspector]
+        public Transform pivot;
+        [HideInInspector]
+        public Transform camTrans;
+
+        private float turnSmoothing = .1f;
+        public float minAngle = -35;
+        public float maxAngle = 35;
+
+        private float smoothX;
+        private float smoothXVelocity;
+        private float smoothY;
+        private float smoothYVelocity;
+        [SerializeField] private float lookAngle;
+        [SerializeField] private float tiltAngle;
+
+        private void Awake () {
+            singleton = this;
+        } // Awake
+
+        public void Init (Transform t) {
+            target = t;
+
+            camTrans = Camera.main.transform;
+            pivot = camTrans.parent;
+        } // Init
+
+        public void Tick (float d) {
+            float h = Input.GetAxis ("Mouse X");
+            float v = Input.GetAxis ("Mouse Y");
+
+            float c_h = Input.GetAxis ("RightAxis X");
+            float c_v = Input.GetAxis ("RightAxis Y");
+
+            float targetSpeed = mouseSpeed;
+
+            if (c_v != 0 || c_h != 0) {
+                h = c_h;
+                v = c_v;
+                targetSpeed = controllerSpeed;
+            }
+
+            HandlePosition (d);
+            HandleRotation (d, v, h, targetSpeed);
+        } // FixedTick
+
+        private void HandleRotation (float d, float v, float h, float targetSpeed) {
+            if (turnSmoothing > 0) {
+                smoothX = Mathf.SmoothDamp (smoothX, h, ref smoothXVelocity, turnSmoothing);
+                smoothY = Mathf.SmoothDamp (smoothY, v, ref smoothYVelocity, turnSmoothing);
+            } else {
+                smoothX = h;
+                smoothY = v;
+            }
+
+            if (lockOn) {
+                Debug.Log ("Not implemented yet!");
+            }
+
+            lookAngle += smoothX * targetSpeed;
+            transform.rotation = Quaternion.Euler (0, lookAngle, 0);
+
+            tiltAngle -= smoothY * targetSpeed;
+            tiltAngle = Mathf.Clamp (tiltAngle, minAngle, maxAngle);
+            pivot.localRotation = Quaternion.Euler (tiltAngle, 0, 0);
+        } // HandleRotation
+
+        private void HandlePosition (float d) {
+            float speed = d * followSpeed;
+            Vector3 targetPosition = Vector3.Lerp (transform.position, target.position, speed);
+            transform.position = targetPosition;
+        } // HandlePosition
+
+    } // Class CameraManager
+
+} // Namespace Arkayns
---
+++ Souls Like Archive/Assets/Scripts/Controller/StateManager.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+
+namespace Arkayns {
+
+    public class StateManager : MonoBehaviour {
+        
+        [Header ("Init")]
+        public GameObject activeModel;
+
+        [Header ("Inputs")]
+        [Range (-1, 1)] public float vertical;
+        [Range (-1, 1)] public float horizontal;
+        public float moveAmount;
+        public Vector3 moveDir;
+
+        [Header ("Stats")]
+        public float moveSpeed = 2;
+        public float runSpeed = 3.5f;
+        public float rotateSpeed = 5;
+        public float toGround = 0.5f;
+
+        [Header ("States")]
+        public bool onGround;
+        public bool run;
+        public bool lockOn;
+
+        [HideInInspector]
+        public Animator anim;
+        [HideInInspector]
+        public Rigidbody rigid;
+
+        [HideInInspector]
+        public float delta;
+        [HideInInspector]
+        public LayerMask ignoreLayers;
+
+        public void Init () {
+            SetupAnimator ();
+            rigid = GetComponent<Rigidbody> ();
+            rigid.angularDrag = 999;
+            rigid.drag = 4;
+            rigid.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+
+            gameObject.layer = 8;
+            ignoreLayers = ~(1 << 9);
+
+            
+        } // Init
+
+        private void SetupAnimator () {
+            if (activeModel == null) {
+                anim = GetComponentInChildren<Animator> ();
+                activeModel = anim.gameObject;
+
+                if (anim == null)
+                    Debug.Log ("No model found");
+            } else {
+                activeModel = anim.gameObject;
+            }
+
+            if (anim == null)
+                anim = activeModel.GetComponent<Animator> ();
+        } // SetupAnimator
+
+        public void FixedTick (float d) {
+            delta = d;
+
+            rigid.drag = (moveAmount > 0 || onGround == false) ? 0 : 4;
+
+            float targetSpee = moveSpeed;
+            if (run)
+                targetSpee = runSpeed;
+
+            if (onGround)
+                rigid.velocity = moveDir * (targetSpee * moveAmount);
+
+            if (run)
+                lockOn = false;
+
+            if (!lockOn) {
+                Vector3 targetDir = moveDir;
+                targetDir.y = 0;
+                if (targetDir == Vector3.zero)
+                    targetDir = transform.forward;
+                Quaternion targetRot = Quaternion.LookRotation (targetDir);
+                Quaternion targetRotation = Quaternion.Slerp (transform.rotation, targetRot, delta * moveAmount * rotateSpeed);
+                transform.rotation = targetRotation;
+            }
+
+            HandleMovementAnimations ();
+        } // FixedTick
+
+        public void Tick (float d) {
+            delta = d;
+            onGround = OnGround ();
+
+            anim.SetBool ("onGround", onGround);
+        } // Tick
+
+        private void HandleMovementAnimations () {
+            anim.SetBool ("run", run);
+            anim.SetFloat ("vertical", moveAmount, 0.4f, delta);
+        } // HandleMovementAnimations
+
+        public bool OnGround () {
+            bool r = false;
+
+            Vector3 origin = transform.position + (Vector3.up * toGround);
+            Vector3 dir = -Vector3.up;
+            float dis = toGround + 0.3f;
+            RaycastHit hit;
+
+            Debug.DrawRay (origin, dir * dis);
+            if (Physics.Raycast(origin, dir, out hit, dis, ignoreLayers)) {
+                r = true;
+                Vector3 targetPosition = hit.point;
+                transform.position = targetPosition;
+            }
+
+            return r;
+        } // OnGround
+
+    } // Class StateManager
+
+} // Namespace Arkayns
